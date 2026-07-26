f3949aa8c32ab637959ce667f6bda5732ad1f7de
sasha ver <samishka53@gmail.com>
2026-07-27 01:59:58 +0400

    классы

--- classes.py
+++ classes.py
@@ -1,8 +1,14 @@
+from datetime import datetime
+
+
 class TaskStatus: # статус выполнения задачи
     Pending = 'pending'
     Processing = 'processing'
     Completed = 'completed'
     Failed = 'failed'
+class TransactionType:
+    Deposit = "deposit"        
+    Charge = "charge"  
 class User:
     # Пользователь
     def __init__(self, id, login, password, role, balance = 0.0):
@@ -15,18 +21,26 @@ class User:
         self.password = password
         self.role = role
         self.balance = balance
-    def added(self, amount): #метод для добавления монет
+        self.transactions = []
+    def deposit(self, amount): #метод для добавления монет
         print(f'монет сейчас до прибавления: {self.balance}')
         self.balance = self.balance + amount
         print(f'добавлено {amount} монет')
         print(f'общая сумма {self.balance}')
-    def use(self, amount2): #метод для использования вычитания монет
+        transaction = Transaction(type_of_tran=TransactionType.Deposit, summa=amount, date_time=datetime.now(), user_id=self)
+        self.transactions.append(transaction)
+        return transaction
+
+    def charge(self, amount2): #метод для использования вычитания монет
         print(f'монет до вычитания: {self.balance}')
         if self.balance < amount2:
             print(f'недостаточно баланса :(, сейчас монет:{self.balance}, нужно не меньше: {amount2}')
         else:
             self.balance = self.balance - amount2
             print(f'все успешно! сейчас ваш баланс: {self.balance}!')
+        transaction = Transaction(type_of_tran=TransactionType.Charge, summa=amount2, date_time=datetime.now(), user_id=self)
+        self.transactions.append(transaction)
+        return transaction
 
 class ML_model: # ML_модель
     def __init__(self, id_model, description, cost_predict):
@@ -38,18 +52,31 @@ class ML_model: # ML_модель
         return {'digit': digit, 'confidence': confidence}
 
 class ML_task: # МL-задача
-    def __init__(self, data, ref_us, ref_ml):
+    def __init__(self, data, status, ref_us, ref_ml):
         # входные данные, статус выполнения, ссылка на пользователя, ссылка на ML-модель
         self.data = data
+        self.status = TaskStatus.Pending
         self.ref_us = ref_us
         self.ref_ml = ref_ml
-    def run(self, ):
-        if self.balance < self.cost_predict:
-            print('недостаточный баланса для выполнения предсказания').Failed
+        self.result = None
+    def run(self, model):
+        if self.ref_us.balance < self.ref_ml.cost_predict:
+            print('недостаточный баланса для выполнения предсказания')
+            self.status = TaskStatus.Failed
+        elif self.balance > self.cost_predict:
+            self.status = TaskStatus.Processing
+            self.result = model.predict(self.data)
+            self.status = TaskStatus.Completed
+            self.balance = self.balance - self.cost_predict
+        else:
+            self.status = TaskStatus.Failed
+
+class Transaction:
+    def __init__(self, type_of_tran, summa, date_time, user_id):
+        self.type_of_tran = type_of_tran
+        self.summa = summa
+        self.date_time = date_time
+        self.user_id = user_id
         
 
 
-chel = User(123, "qwe123!", "qwerty123", "defolt", 10)
-chel = ML_task(1, 2, 3)
-
-chel.run()
