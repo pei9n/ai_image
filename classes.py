baf3948b484c00c3c9e2aaaac6ef635b1eed6229
sasha ver <samishka53@gmail.com>
2026-07-28 19:20:31 +0400

    классы

--- classes.py
+++ classes.py
@@ -6,12 +6,15 @@ class TaskStatus: # статус выполнения задачи
     Processing = 'processing'
     Completed = 'completed'
     Failed = 'failed'
+
+
 class TransactionType:
     Deposit = "deposit"        
-    Charge = "charge"  
-class User:
-    # Пользователь
-    def __init__(self, id, login, password, role, balance = 0.0):
+    Charge = "charge"
+
+
+class User: # Пользователь
+    def __init__(self, id: int, login: str, password: str, role: str, balance = 0.0): 
         # id - идентификатор пользователя
         # login, password - данные для авторизации
         # role - роль пользователя 
@@ -22,28 +25,37 @@ class User:
         self.role = role
         self.balance = balance
         self.transactions = []
-    def deposit(self, amount): #метод для добавления монет
-        print(f'монет сейчас до прибавления: {self.balance}')
-        self.balance = self.balance + amount
+
+
+class Wallet:
+    def __init__(self, balance = 0):
+        self.__balance = balance
+        self.transactions = []
+
+    def deposit(self, amount: int, user_id: int) -> bool: #метод для добавления монет
+        print(f'монет сейчас до прибавления: {self.__balance}')
+        self.__balance = self.__balance + amount
         print(f'добавлено {amount} монет')
-        print(f'общая сумма {self.balance}')
-        transaction = Transaction(type_of_tran=TransactionType.Deposit, summa=amount, date_time=datetime.now(), user_id=self)
+        print(f'общая сумма {self.__balance}')
+        transaction = Transaction(type_of_tran=TransactionType.Deposit, summa=amount, date_time=datetime.now(), user_id=user_id)
         self.transactions.append(transaction)
         return transaction
 
-    def charge(self, amount2): #метод для использования вычитания монет
-        print(f'монет до вычитания: {self.balance}')
-        if self.balance < amount2:
-            print(f'недостаточно баланса :(, сейчас монет:{self.balance}, нужно не меньше: {amount2}')
+    def charge(self, amount2: int, user_id: int) -> bool: #метод для использования вычитания монет
+        print(f'монет до вычитания: {self.__balance}')
+        if self.__balance < amount2:
+            print(f'недостаточно баланса :(, сейчас монет:{self.__balance}, нужно не меньше: {amount2}')
+            return False
         else:
-            self.balance = self.balance - amount2
-            print(f'все успешно! сейчас ваш баланс: {self.balance}!')
-        transaction = Transaction(type_of_tran=TransactionType.Charge, summa=amount2, date_time=datetime.now(), user_id=self)
+            self.__balance = self.__balance - amount2
+            print(f'все успешно! сейчас ваш баланс: {self.__balance}!')
+        transaction = Transaction(type_of_tran=TransactionType.Charge, summa=amount2, date_time=datetime.now(), user_id=user_id)
         self.transactions.append(transaction)
         return transaction
+
 
 class ML_model: # ML_модель
-    def __init__(self, id_model, description, cost_predict):
+    def __init__(self, id_model: int, description: str, cost_predict: float):
         # идентификатор модели, описание модели, стоимость одного предсказания
         self.id_model = id_model
         self.description = description
@@ -51,32 +63,32 @@ class ML_model: # ML_модель
     def predict(self, image_data): # метод выполнения предсказания 
         return {'digit': digit, 'confidence': confidence}
 
+
 class ML_task: # МL-задача
-    def __init__(self, data, status, ref_us, ref_ml):
+    def __init__(self, data, status: str, user: User, model: ML_model):
         # входные данные, статус выполнения, ссылка на пользователя, ссылка на ML-модель
         self.data = data
         self.status = TaskStatus.Pending
-        self.ref_us = ref_us
-        self.ref_ml = ref_ml
+        self.user = user
+        self.model = model
         self.result = None
-    def run(self, model):
-        if self.ref_us.balance < self.ref_ml.cost_predict:
+
+    def run(self):
+        if self.user._Wallet__balance < self.model.cost_predict:
             print('недостаточный баланса для выполнения предсказания')
             self.status = TaskStatus.Failed
-        elif self.balance > self.cost_predict:
+        elif self.user._Wallet__balance > self.model.cost_predict:
             self.status = TaskStatus.Processing
-            self.result = model.predict(self.data)
+            self.result = self.model.predict(self.data)
             self.status = TaskStatus.Completed
-            self.balance = self.balance - self.cost_predict
+            self.user._Wallet__balance = self.user._Wallet__balance - self.model.cost_predict
         else:
             self.status = TaskStatus.Failed
 
+
 class Transaction:
-    def __init__(self, type_of_tran, summa, date_time, user_id):
+    def __init__(self, type_of_tran: str, summa: int, date_time: datetime, user_id: int):
         self.type_of_tran = type_of_tran
         self.summa = summa
         self.date_time = date_time
         self.user_id = user_id
-        
-
-
